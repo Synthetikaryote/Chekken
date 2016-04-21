6d9501de2ecf726260223da0ec3120a8704a3313
JaF95 <jaffulljames@gmail.com>
2016-04-21 12:19:48 -0700

    Health, cooldown, name, size update
    Added health bar and cooldown bar systems
    Added chick name
    Changed the scale of the chick prefab

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+    //Public members
+    public Color fullHealthColour;
+    public Color lowHealthColour;
+    public Vector3 offset;
+
+    //Private member
+    private HealthSystem chickHealth;
+    private Renderer barRenderer;
+    private Transform chickTransform;
+
+    void Awake()
+    {
+        chickHealth = GetComponentInParent<HealthSystem>();
+        barRenderer = GetComponent<Renderer>();
+        chickTransform = GetComponentInParent<Transform>();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	    if (!chickHealth)
+        {
+            Debug.LogError("[HealthBar.cs] Cannot find the chick's health!");
+        }
+        if (!barRenderer)
+        {
+            Debug.LogError("[HealthBar.cs] Cannot find the health bar's renderer!");
+        }
+        if (!chickTransform)
+        {
+            Debug.LogError("[HealthBar.cs] Cannot find the chick's transform!");
+        }
+
+        transform.localPosition += offset;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {       
+        transform.localScale = new Vector3(chickHealth.GetCurHealth() / 166.7f, 0.1f, 0.000001f);
+
+        Color barColour = Color.Lerp(lowHealthColour, fullHealthColour, (chickHealth.GetCurHealth() / chickHealth.maxHealth));
+        barRenderer.material.SetColor("_Color", barColour);
+	}
+}
---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthSystem : MonoBehaviour
+{
+    //Public members
+    public float maxHealth;
+
+    //Private members
+    private float curHealth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        curHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {    }
+
+    //Health funcs
+    public void TakeDamage(float damage)
+    {
+        curHealth -= damage;
+    }
+    public float GetCurHealth()
+    {
+        return curHealth;
+    }
+}
