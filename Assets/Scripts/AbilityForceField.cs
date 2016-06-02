596e8a2d457b914b39dc3478763fac5a9b61e573
VictorwlChoi <vncvrvictorchoi@outlook.com>
2016-06-02 12:13:02 -0700

    ForceField Script Addition

--- Assets/Scripts/AbilityForceField.cs
+++ Assets/Scripts/AbilityForceField.cs
@@ -3,31 +3,27 @@ using System.Collections;
 
 public class AbilityForceField : AbilityBaseClass
 {
-    GameObject mProjectile;
-    public Vector3 mOffset;
+    public GameObject mPrefab;
+    public GameObject mShield;
+    public GameObject mPlayer;
     public float mForce;
     private Vector3 mForceVector;
-
-    //Chicken Animator 
-    Animator mAnimator;
-    int attack = Animator.StringToHash("Attack");
-    int damage = Animator.StringToHash("Damage");
-
-    //Chicken Attack Audio
-    AudioSource AttackAudio;
-    AudioSource DamageAudio;
+    
+    
     // Use this for initialization
 
     public override void Initialize()
     {
         mCC = GetComponent<ChickController>();
         mCDown = 5.0f;//200.0f;
-        
+        mShield = GameObject.Instantiate(mPrefab, this.transform.position, this.transform.rotation) as GameObject;
+        mShield.transform.parent = mPlayer.transform;
     }
 
     public override void ActivateAbility()
     {
-        
+
+        mCC.SetCoolDown(mCDown);
     }
 
 }
