7cced569ee07fa8822200167c7702aa679478599
Hendry Kuo <hendrykuo@stu.aii.edu>
2016-06-02 12:40:46 -0700

    new message UI

---
+++ Assets/Scripts/ChatBox.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ChatBox : MonoBehaviour
+{
+    [SerializeField] ContentSizeFitter contentSizeFitter;
+    [SerializeField] Text showhideButtonText;
+    [SerializeField] Transform messageParentPanel;
+    [SerializeField] GameObject newMessagePrefab;
+    bool showChat = false;
+    string message = "";
+    void Start()
+    {
+        ToggleChat();
+    }
+    public void ToggleChat()
+    {
+        showChat = !showChat;
+        if (showChat)
+        {
+            contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            showhideButtonText.text = "hide chat";
+        }
+        else
+        {
+            contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.MinSize;
+            showhideButtonText.text = "show chat";
+        }
+    }
+
+    public void SetMessage(string message)
+    {
+        this.message = message;
+    }
+
+    public void ShowMessage()
+    {
+        if (message != "")
+        {
+            GameObject clone = Instantiate<GameObject>(newMessagePrefab);
+            clone.transform.SetParent(messageParentPanel);
+            clone.transform.SetSiblingIndex(messageParentPanel.childCount - 2);
+            clone.GetComponent<Message>().ShowMessage(message);
+        }
+    }
+
+}
---
+++ Assets/Scripts/DragableWindow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class DragableWindow : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    public Vector3 offset;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        offset = transform.position - (Vector3)eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = (Vector3)eventData.position + offset;
+    }
+}
